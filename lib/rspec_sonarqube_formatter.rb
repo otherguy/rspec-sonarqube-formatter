9c429deb644d76db18bba1e424649fe0fcd7fe8c
Alexander Graf <hi@basecamp.tirol>
2019-12-13 14:39:00 +0100

    Add HTMLEntity encode

--- lib/rspec_sonarqube_formatter.rb
+++ lib/rspec_sonarqube_formatter.rb
@@ -39,7 +39,7 @@ class RspecSonarqubeFormatter
 
   def example_failed(notification)
     @output.puts "    <testCase name=\"#{clean_string(notification.example.description)}\" duration=\"#{duration(notification.example)}\">"
-    @output.puts "      <failure message=\"#{notification.exception}\" stacktrace=\"#{notification.example.location}\" />"
+    @output.puts "      <failure message=\"#{clean_string(notification.exception)}\" stacktrace=\"#{clean_string(notification.example.location)}\" />"
     @output.puts '    </testCase>'
   end
 
@@ -50,7 +50,7 @@ class RspecSonarqubeFormatter
   end
 
   def clean_string(input)
-    input.to_s.gsub(/\e\[\d;*\d*m/, '').tr('"', "'")
+    HTMLEntities.new.encode input.to_s.gsub(/\e\[\d;*\d*m/, '').tr('"', "'")
   end
 
   def duration(example)
